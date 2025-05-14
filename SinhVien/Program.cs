983cd4b091717a05bb34c8a91ceae656db458951
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2025-05-14 10:52:50 +0700

    Update Program.cs

--- SinhVien/Program.cs
+++ SinhVien/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SinhVien
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            List<Student> danhSachSV = new List<Student>();
+
+            Console.WriteLine("=== Nhập thông tin cho 3 sinh viên ===\n");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"--- Sinh viên thứ {i + 1} ---");
+                Student sv = new Student();
+                sv.NhapThongTin();
+                danhSachSV.Add(sv);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n=== Thông tin và xếp loại sinh viên ===\n");
+
+            foreach (var sv in danhSachSV)
+            {
+                sv.HienThiThongTin();
+            }
+        }
     }
 }

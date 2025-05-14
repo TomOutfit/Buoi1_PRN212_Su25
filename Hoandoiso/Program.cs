2fcae116991dcae6ddc346d6f9559450acaa08ab
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2025-05-14 10:46:27 +0700

    Bổ sung code bài Hoán đổi số

--- Hoandoiso/Program.cs
+++ Hoandoiso/Program.cs
@@ -1,8 +1,28 @@
-﻿namespace Hoandoiso
+﻿using System;
+
+namespace Hoandoiso
 {
     public class Program
     {
-        // 1. Viết một hàm HoanDoi(ref int a, ref int b)
-  //để hoán đổi giá trị của hai biến và in kết quả trong Main//
+        // Hàm hoán đổi giá trị hai biến sử dụng ref
+        public static void HoanDoi(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhập số thứ nhất: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhập số thứ hai: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nTrước khi hoán đổi: x = {x}, y = {y}");
+            HoanDoi(ref x, ref y);
+            Console.WriteLine($"Sau khi hoán đổi: x = {x}, y = {y}");
+        }
     }
 }

d2fa7a79694b59df6ee937742dc9a520de5141e4
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2025-05-14 10:51:14 +0700

    Update Program.cs

--- HinhChuNhat/Program.cs
+++ HinhChuNhat/Program.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace HinhChuNhat
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            Rectangle hcn = new Rectangle();
 
+            Console.Write("Nhập chiều dài: ");
+            hcn.ChieuDai = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập chiều rộng: ");
+            hcn.ChieuRong = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nThông tin hình chữ nhật:");
+            hcn.HienThiThongTin();
+        }
     }
 }

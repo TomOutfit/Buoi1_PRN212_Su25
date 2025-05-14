c01eb7c27aac1aeafe8307a52b08069f13052694
TomOutfit <167419245+TomOutfit@users.noreply.github.com>
2025-05-14 10:38:18 +0700

    Add files via upload
    Thêm bài 1.1 - Hoán đổi số

---
+++ Hoandoiso/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Hoandoiso
+{
+    public class Program
+    {
+        // 1. Viết một hàm HoanDoi(ref int a, ref int b)
+  //để hoán đổi giá trị của hai biến và in kết quả trong Main//
+    }
+}
